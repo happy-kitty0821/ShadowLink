81562f6e54266f6e0f6333d37d7aa31ed62261f6
happy-kitty0821 <ayushhaang09@gmail.com>
2025-06-23 22:13:41 +0545

    user can see their user profile and update their profile

--- authentication_app/urls.py
+++ authentication_app/urls.py
@@ -6,7 +6,11 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('', auth_views.userLogin, name = 'login_page'),
     path('register-user', auth_views.registerUser, name = 'register_user'),
-
+    path('user-profile', auth_views.view_profile, name = 'user_profile'),
+    path('user-profile/update', auth_views.update_profile, name = 'update_profile'),
+    path('user-profile/verify-email', auth_views.verify_email, name = 'verify_email'),
+    path('profile/confirm-email/', auth_views.confirm_email, name='confirm_email'),
+    path('user-profile/delete/account', auth_views.userAccountDelete, name = 'delete_account' ),
     #user logout view
     path('logout/', LogoutView.as_view(), name='logout'),
 
